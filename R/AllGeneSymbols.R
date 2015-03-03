03497c3c4e4491d5fd0503af31b16268228f61bb
Zhilong Jia <zhilongjia@gmail.com>
2015-03-03 17:31:28 +0000

    add the backgound gene set, AllGeneSymbols.

---
+++ R/AllGeneSymbols.R
@@ -0,0 +1,16 @@
+#' All GENE SYMBOLS
+#' 
+#' This is all the gene symbols of protein-coding genes. There are 18986 genes
+#' totally, which are used as the backgound genes usually.
+#' 
+#' 
+#' There are one object.
+#' \itemize{
+#'  \item AllGeneSymbols a vector of gene symbols.
+#' }
+#' @docType data
+#' @keywords datasets
+#' @name AllGeneSymbols
+#' @usage data(AllGeneSymbols)
+#' @source \url{http://www.genenames.org/cgi-bin/statistics}
+NULL
