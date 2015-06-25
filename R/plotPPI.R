0a7083672e9503c637f01184daee7b6020d580dd
Zhilong Jia <zhilongjia@gmail.com>
2015-06-25 15:19:35 +0100

    add plotPPI function.

---
+++ R/plotPPI.R
@@ -0,0 +1,47 @@
+#' Plot the Protein-protein interaction for genes in a certain cluster.
+#' 
+#' Show how genes in a certain cluster are interacted based on String database.
+#'
+#' @inheritParams enrichment
+#' @param ith the ith cluster.
+#' @param score_threshold a threshold for the combined scores of the
+#'  interactions. Default is 0.
+#' @param ... parameter to plot_network from STRINGdb
+#' @return a figure
+#' @rdname plotPPI
+#' @import STRINGdb
+#' @export
+#' @seealso \code{\link{cogena}}
+#' @examples
+#' data(PD)
+#' annofile <- system.file("extdata", "c2.cp.kegg.v4.0.symbols.gmt", 
+#' package="cogena")
+#' cogena_result <- cogena(DEexprs, nClust=2:3, 
+#' clMethods=c("hierarchical","kmeans"), metric="correlation", 
+#' method="complete",  annofile=annofile, sampleLabel=sampleLabel, 
+#' ncore=1, verbose=TRUE)
+#' #summay this cogena object
+#' summary(cogena_result)
+#' 
+#' #plotPPI
+#' plotPPI(cogena_result, "kmeans", "3", "3")
+#' 
+#' 
+setGeneric("plotPPI", function(object, method, nClusters, ith, score_threshold=0, ...) 
+    standardGeneric("plotPPI"))
+
+#' @rdname plotPPI
+#' @aliases plotPPI,cogena_methods
+setMethod("plotPPI", signature(object="cogena"),
+          function (object, method=clusterMethods(object), 
+                    nClusters=nClusters(object), ith,
+                    score_threshold=0, ...){
+    geneC <- geneInCluster(object, method, nClusters, as.character(ith))
+    suppressWarnings(string_db <- STRINGdb$new(version="10", species=9606, score_threshold=score_threshold))
+    
+    example1_mapped <- string_db$map(as.data.frame(geneC), "geneC", removeUnmappedRows = TRUE, quiet=TRUE)
+    hits <- example1_mapped$STRING_id
+    string_db$plot_network(hits, ...)
+    }
+)
+
