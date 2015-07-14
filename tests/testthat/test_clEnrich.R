61ae94ddc5ba2c15fc6bd7ad2e10d6b68c1e3506
Zhilong Jia <zhilongjia@gmail.com>
2015-07-14 21:17:23 +0100

    make tests flexible.

--- tests/testthat/test_clEnrich.R
+++ tests/testthat/test_clEnrich.R
@@ -1,6 +1,6 @@
 library(cogena)
 data(PD)
-clMethods <- c("hierarchical","kmeans","diana","fanny","som","model","sota","pam","clara","agnes")
+# clMethods <- c("hierarchical","kmeans","diana","fanny","som","model","sota","pam","clara","agnes")
 clMethods <- c("hierarchical","kmeans")
 
 annofile <- system.file("extdata", "c2.cp.kegg.v5.0.symbols.gmt", package="cogena")
@@ -8,7 +8,7 @@ annofile <- system.file("extdata", "c2.cp.kegg.v5.0.symbols.gmt", package="cogen
 context("Testing coExp, clEnrich and optCluster\n")
 
 test_that("coExp, clEnrich and optCluster", {
-	expect_is(genecl_result <- coExp(DEexprs, nClust=2:3, clMethods=clMethods, metric="correlation", method="complete", ncore=2, verbose=TRUE), "genecl")
+    expect_is(genecl_result <- coExp(DEexprs, nClust=2:3, clMethods=clMethods, metric="correlation", method="complete", ncore=2, verbose=TRUE), "genecl")
     expect_is(clen_res <- clEnrich(genecl_result, annofile=annofile, sampleLabel=sampleLabel, ncore=2),
               "cogena")
     expect_is(optCluster(clen_res, ncores=2), "matrix")
