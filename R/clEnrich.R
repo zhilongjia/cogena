b98446581200499ff859928078180c8f12eb1855
Jia Zhilong <zhilongjia@gmail.com>
2015-07-14 20:28:35 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 155e924cdec7d4b713b0978dd5b7cd81bfe03791
    
        log2 instead of log in clEnrich.
    
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/cogena@106382 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/clEnrich.R
+++ R/clEnrich.R
@@ -80,12 +80,12 @@ clEnrich <- function(genecl_obj, annofile=NULL, sampleLabel=NULL, TermFreq=0, nc
                                                  annotationGenesPop=annotationGenesPop)
                 }
                 pei <- rbind(pei, All)
-                # negative log p value
+                # negative log2 p value
                 logAdjPEI <- function (pei) {
                     # fdr based on pval
                     pei.adjust <- matrix(p.adjust(pei, "fdr"), ncol=ncol(pei))
                     dimnames(pei.adjust) <- dimnames(pei)
-                    pei.NeglogPval <- -log(pei.adjust)
+                    pei.NeglogPval <- -log2(pei.adjust)
                 }
                 pei <- logAdjPEI(pei)
             }
