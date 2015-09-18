6fc48c6c6583789dbe2ffb8f1f97a2752e814622
Jia Zhilong <zhilongjia@gmail.com>
2015-09-18 17:47:49 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 5d28d152c739d951b370fa586777491eb3497307
    
        update breaks in heatmapPEI and heatmapCmap.
    
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/cogena@108644 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/heatmapPEI.R
+++ R/heatmapPEI.R
@@ -110,9 +110,9 @@ setMethod("heatmapPEI", signature(object="cogena"),
         cutoff_score <- round(-log2(CutoffPVal), 2)
         max_score <- max(enrichment_score$value, na.rm=TRUE)
         if (max_score/cutoff_score > 2) {
-            breaks <- round( seq(cutoff_score, max_score, length.out=5), 2)
+            breaks <- c(cutoff_score, round( seq(cutoff_score, max_score, length.out=5)[-1]))
         } else if (max_score/cutoff_score >1) {
-            breaks <- round( seq(cutoff_score, max_score, length.out=2), 2)
+            breaks <- c(cutoff_score, round( seq(cutoff_score, max_score, length.out=2)[-1]))
         } else {
             breaks <- NULL
         }
