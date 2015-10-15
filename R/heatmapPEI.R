f07f24874dc2a624ab68087bc3d3ae7198672305
z.jia <z.jia@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-10-15 11:43:02 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: c71b4df84350f081faaa44db8fb697e93df089c9
    
        add vjust in heatmapPEI/Cmap and explicitly factor sample labels with levels.
    
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/cogena@109646 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/heatmapPEI.R
+++ R/heatmapPEI.R
@@ -132,7 +132,7 @@ setMethod("heatmapPEI", signature(object="cogena"),
             labs(list(title = title, x = "Cluster", y = "Gene set")) +
             theme(axis.text.y = element_text(size = rel(1.5), face="bold")) +
             theme(axis.text.x = element_text(size = rel(1.3), angle=-90, 
-                face="bold", color=cl_color)) +
+                face="bold", color=cl_color, vjust=0.5)) +
             theme(panel.grid.major.x = element_line(color = "grey", size = 5),
                   panel.grid.major.y = element_blank())
     }
