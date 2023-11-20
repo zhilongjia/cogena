7ed18cb7e06c96124a8f2cafc57644728df15037
zhilongjia <zhilongjia@gmail.com>
2023-11-20 15:25:31 +0800

    add description in gmt and delete finename if exists.

--- R/gmtlist2file.R
+++ R/gmtlist2file.R
@@ -4,6 +4,8 @@
 #' 
 #' @param gmtlist a list containing gmt
 #' @param filename output filename
+#' @param description a detailed description vector of a gene set this line. 
+#' length of the vector should be the same as the length of gmtlist.
 #' @return NA
 #' 
 #' @export
@@ -13,12 +15,26 @@
 #' anno <- "c2.cp.kegg.v7.01.symbols.gmt.xz"
 #' annofile <- system.file("extdata", anno, package="cogena")
 #' gl <- gmt2list(annofile)
-#' gmtfile <- gmtlist2file(gl, filename="")
+#' gmtfile <- gmtlist2file(gl, filename="" )
 #' 
-gmtlist2file <- function(gmtlist, filename){
+gmtlist2file <- function(gmtlist, filename, description=NULL){
     gsname=names(gmtlist) 
-    for (i in seq_along(gmtlist) ){ 
-        cat(gsname[i], length(gmtlist[[i]]), gmtlist[[i]], file=filename, append=TRUE, sep = "\t")
-        cat("\n", append=TRUE, file=filename)
+    
+    if (file.exists(filename) ) { file.remove(filename) }
+    
+    if (is.null(description)) {
+        for (i in seq_along(gmtlist) ){ 
+            cat(gsname[i], length(gmtlist[[i]]), gmtlist[[i]], file=filename, append=TRUE, sep = "\t")
+            cat("\n", append=TRUE, file=filename)
+        }
+    } else if (length(description) == length(gmtlist) ) {
+        description <- gsub("\r\n|\r|\n","",description)
+        for (i in seq_along(gmtlist) ){ 
+            cat(gsname[i], description[i], gmtlist[[i]], file=filename, append=TRUE, sep = "\t")
+            cat("\n", append=TRUE, file=filename)
+        }
+    } else {
+        stop("The length of description should be the same as the length of gmtlist.")
     }
+    
 }
